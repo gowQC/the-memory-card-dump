4935edf74857616b45c88feb25a9967f0a81c690
Nicholas Osorio <nicholas.osorio98@gmail.com>
2025-09-02 15:51:35 -0400

    Added routes and working pages

--- frontend-tmcd/src/App.jsx
+++ frontend-tmcd/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router";
+
 // https://tailwindcss.com/docs/installation/using-vite
 // https://tailwind-converter.netlify.app/ - TAILWIND CONVERTER
 
 function App() {
   return (
-    <>
-      <LandingPage></LandingPage>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/registration" element={<RegistrationPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
   );
 }
 
---
+++ frontend-tmcd/src/pages/RegistrationPage.jsx
@@ -0,0 +1,3 @@
+export default function RegisterPage() {
+  return <>this is the registration page</>;
+}
